5223f771f92db12b2a23653dddab087d209612e7
Nicholas Perrotti <nicholas.perrotti79@gmail.com>
2014-01-09 13:57:30 -0500

    1-9-14 Nick

--- WordList.java
+++ WordList.java
@@ -1,30 +1,32 @@
-//Oliver Zhang
-//pd 8
-//2013-12-16
-//HW 44
+//
+
 import java.util.ArrayList;
 public class WordList {
 
     private ArrayList<String> myList; // contains Strings made up of letters
     
     public WordList() {
-	myList = new ArrayList<String>(8);
-	myList.add("dog");
-	myList.add("cat");
-	myList.add("bird");
-	myList.add("mouse");
-	myList.add("cow");
-	myList.add("frog");
-	myList.add("elephant");
-	myList.add("fox");
-	myList.add("dog");
-	myList.add("cat");
-	myList.add("bird");
-	myList.add("mouse");
-	myList.add("cow");
-	myList.add("frog");
-	myList.add("elephant");
-	myList.add("fox");
+	myList = new ArrayList<String>(16);
+	//MOVIE QUOTES
+	myList.add("IT'S NOTHING PERSONAL SONNY, IT'S STRICTLY BUSINESS");
+	myList.add("LUKE, I AM YOUR FATHER");
+	myList.add("WHO YOU GONNA CALL? GHOSTBUSTERS!");
+	myList.add("YOU SHALL NOT PASS!");
+	//ACTORS
+	myList.add("JENNIFER LAWRENCE");
+	myList.add("LEONARDO DI CAPRIO");
+	myList.add("MARILYN MONROE");
+	myList.add("TOM HANKS");
+	//IDIOMS
+	myList.add("RAINING CATS AND DOGS");
+	myList.add("PIECE OF CAKE");
+	myList.add("BREAK A LEG");
+	myList.add("HIT THE HAY");
+	//BRAND NAMES
+	myList.add("BARNES AND NOBLE");
+	myList.add("LOUIS VUITTON");
+	myList.add("GENERAL MILLS");
+	myList.add("AMERICAN AIRLINES");
     }
 
     //postcond: returns the number of words in this WordList that are 
